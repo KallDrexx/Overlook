9980861755788dd4dfd382b7fda827bbda3f78cb
KallDrexx <me@mshapiro.net>
2013-08-13 23:17:05 -0400

    Removed unused database project and sqlite library (nuget one is used instead)

--- Overlook.SqlDatabase/Tables/MetricData.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[MetricData]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Date] INT NOT NULL, 
-    [MetricDevice] NVARCHAR(30) NOT NULL, 
-    [MetricCategory] NVARCHAR(30) NOT NULL, 
-    [MetricName] NVARCHAR(50) NOT NULL, 
-    [SuffixLabel] NVARCHAR(50) NOT NULL
-)
