6f46c242398b66a8061a14f2f15e354634d03c78
KallDrexx <me@mshapiro.net>
2013-08-15 21:50:05 -0400

    Added code to store snapshots every 15 seconds

---
+++ Overlook.Server/ApplicationSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace Overlook.Server
+{
+    public static class ApplicationSettings
+    {
+        public static string DatabaseName { get { return GetSetting<string>("DatabaseFileName"); } }
+        public static int SecondsBetweenSnapshots { get { return GetSetting<int>("SecondsBetweenSnapshots"); } }
+
+        private static T GetSetting<T>(string settingName)
+        {
+            var value = ConfigurationManager.AppSettings[settingName];
+            if (value == null)
+                return default(T);
+
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
--- Overlook.Server/Program.cs
+++ Overlook.Server/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using Overlook.Common.Data;
 using Overlook.Server.MetricRetriever;
+using Overlook.Server.Storage.Sqlite;
 
 [assembly: InternalsVisibleTo("Overlook.Tests")]
 
@@ -11,8 +14,34 @@ namespace Overlook.Server
     {
         static void Main(string[] args)
         {
-            var retriever = new OpenHardwareMonitorMetricRetriever();
-            var metrics = retriever.GetCurrentMetricValues().ToArray();
+            var retrievers = new IMetricRetriever[]
+            {
+                new OpenProcessMetricRetriever(),
+                new OpenHardwareMonitorMetricRetriever()
+            };
+
+            var storageEngine = new SqliteStorageEngine(ApplicationSettings.DatabaseName);
+
+            var lastCheck = DateTime.MinValue;
+            var secondsBetweenChecks = ApplicationSettings.SecondsBetweenSnapshots;
+            while (true)
+            {
+                if ((DateTime.Now - lastCheck).TotalSeconds > secondsBetweenChecks)
+                {
+                    var snapshot = new Snapshot
+                    {
+                        Date = DateTime.Now,
+                        MetricValues = retrievers.SelectMany(x => x.GetCurrentMetricValues())
+                                                 .ToArray()
+                    };
+
+                    storageEngine.StoreSnapshot(snapshot);
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
         }
     }
 }
