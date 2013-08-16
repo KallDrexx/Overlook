904ebdc1f005c73f1a7a9bd8a5a1c151389e2620
KallDrexx <me@mshapiro.net>
2013-08-15 21:03:55 -0400

    Added metric retriever to get names of the active processes

---
+++ Overlook.Server/MetricRetriever/OpenProcessMetricRetriever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Overlook.Common.Data;
+
+namespace Overlook.Server.MetricRetriever
+{
+    public class OpenProcessMetricRetriever : IMetricRetriever
+    {
+        public IEnumerable<KeyValuePair<Metric, decimal>> GetCurrentMetricValues()
+        {
+            const string device = "Processes";
+            const string openCategory = "Open";
+
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                var name = process.ProcessName;
+                var metric = new Metric(device, openCategory, name, string.Empty);
+                yield return new KeyValuePair<Metric, decimal>(metric, 1m);
+            }
+        }
+    }
+}
---
+++ Overlook.Tests/MetricRetrieverTests/OpenProcessMetricRetrieverTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Overlook.Server.MetricRetriever;
+
+namespace Overlook.Tests.MetricRetrieverTests
+{
+    [TestFixture]
+    class OpenProcessMetricRetrieverTests
+    {
+        [Test]
+        public void Can_Get_Running_Processes()
+        {
+            var retriever = new OpenProcessMetricRetriever();
+            var metrics = retriever.GetCurrentMetricValues();
+            
+            // Since we can't gaurantee what processes will be shown, just 
+            // make sure we at least metrics were returned
+            Assert.IsNotNull(metrics, "Metric enumerable was null");
+            Assert.IsNotEmpty(metrics, "Metric enumerable was empty");
+        }
+    }
+}
