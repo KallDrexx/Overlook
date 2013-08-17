1cdbe4ff3988754f88b3357fd2d39a570d77acc1
KallDrexx <me@mshapiro.net>
2013-08-17 14:11:13 -0400

    Updated metric module tests to verify the correct results are returned in the http response

--- Overlook.Tests/Web/MetricsModuleTests.cs
+++ Overlook.Tests/Web/MetricsModuleTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Nancy;
-using Nancy.Bootstrapper;
 using Nancy.Testing;
+using Newtonsoft.Json;
+using System.Linq;
+using Overlook.Common.Data;
 using Overlook.Server.Storage;
 using Overlook.Server.Web;
 
@@ -12,29 +14,33 @@ namespace Overlook.Tests.Web
     public class MetricsModuleTests
     {
         private Mock<IStorageEngine> _storageEngine;
-        private INancyBootstrapper _bootstrapper;
         private Browser _browser;
 
         [TestInitialize]
         public void Init()
         {
             _storageEngine = new Mock<IStorageEngine>();
-            _bootstrapper = new OverlookBootStrapper(_storageEngine.Object);
-            _browser = new Browser(_bootstrapper);
+
+             var bootstrapper = new OverlookBootStrapper(_storageEngine.Object);
+             _browser = new Browser(bootstrapper);
         }
 
         [TestMethod]
-        public void List_Returns_OK_Response()
+        public void List_Returns_Correct_Metrics()
         {
+            var metric1 = new Metric("device1", "category1", "name1", "suffix");
+            var metric2 = new Metric("device2", "category2", "name2", "suffix");
+            _storageEngine.Setup(x => x.GetKnownMetrics()).Returns(new[] { metric1, metric2 });
+
             var result = _browser.Get("/metrics/list", with => with.HttpRequest());
-            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode, "Incorrect status code returned");
-        }
+            var metrics = JsonConvert.DeserializeObject<Metric[]>(result.Body.AsString());
 
-        [TestMethod]
-        public void List_Gets_Metrics_From_Storage_Engine()
-        {
-            _browser.Get("/metrics/list", with => with.HttpRequest());
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode, "Incorrect status code returned");
             _storageEngine.Verify(x => x.GetKnownMetrics(), Times.Once());
+            Assert.IsNotNull(metrics, "Metrics array was null");
+            Assert.AreEqual(2, metrics.Length, "Metrics array length was incorrect");
+            Assert.IsTrue(metrics.Contains(metric1), "First metric is not in the returned array");
+            Assert.IsTrue(metrics.Contains(metric2), "Second metric is not in the returned array");
         }
     }
 }
