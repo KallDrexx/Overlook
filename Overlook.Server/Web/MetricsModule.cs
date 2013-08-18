399934fd0a4b51ee9f48559c09ad54c230361ff9
KallDrexx <me@mshapiro.net>
2013-08-18 17:01:09 -0400

    Added code to respond to /metrics/query requests

--- Overlook.Server/Web/MetricsModule.cs
+++ Overlook.Server/Web/MetricsModule.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Nancy;
+using Nancy.ModelBinding;
 using Overlook.Common.Data;
+using Overlook.Common.Queries;
 using Overlook.Server.Storage;
 
 namespace Overlook.Server.Web
@@ -13,13 +16,28 @@ namespace Overlook.Server.Web
         {
             _storageEngine = storageEngine;
 
-            Get["/list"] = parameters => Response.AsJson(GetAvailableMetrics());
-        }
+            Get["/list"] = parameters =>
+            {
+                var metrics = _storageEngine.GetKnownMetrics();
+                return Response.AsJson(metrics.ToArray());
+            };
 
-        private Metric[] GetAvailableMetrics()
-        {
-            var metrics = _storageEngine.GetKnownMetrics();
-            return metrics.ToArray();
+            Get["/query"] = parameters =>
+            {
+                DateTime start, end;
+                DateTime.TryParse(Request.Query.start, out start);
+                DateTime.TryParse(Request.Query.end, out end);
+                var metric = Metric.Create(Request.Query.metrics);
+                var query = new Query
+                {
+                    StartDate = start,
+                    EndDate = end,
+                    Metrics = new Metric[] {metric}
+                };
+
+                var results = _storageEngine.ExecuteQuery(query);
+                return Response.AsJson(results.ToArray());
+            };
         }
     }
 }
--- Overlook.Tests/Web/MetricsModuleTests.cs
+++ Overlook.Tests/Web/MetricsModuleTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Nancy;
 using Nancy.Testing;
 using Newtonsoft.Json;
 using System.Linq;
 using Overlook.Common.Data;
+using Overlook.Common.Queries;
 using Overlook.Server.Storage;
 using Overlook.Server.Web;
 
@@ -21,8 +24,8 @@ namespace Overlook.Tests.Web
         {
             _storageEngine = new Mock<IStorageEngine>();
 
-             var bootstrapper = new OverlookBootStrapper(_storageEngine.Object);
-             _browser = new Browser(bootstrapper);
+            var bootstrapper = new OverlookBootStrapper(_storageEngine.Object);
+            _browser = new Browser(bootstrapper);
         }
 
         [TestMethod]
@@ -41,6 +44,60 @@ namespace Overlook.Tests.Web
             Assert.AreEqual(2, metrics.Length, "Metrics array length was incorrect");
             Assert.IsTrue(metrics.Contains(metric1), "First metric is not in the returned array");
             Assert.IsTrue(metrics.Contains(metric2), "Second metric is not in the returned array");
+        }
+
+        [TestMethod]
+        public void Query_Builds_Correct_Query_From_Parameters()
+        {
+            var metric = new Metric("device", "category", "name", "suffix");
+            var path = "/metrics/query";
+            var startDate = DateTime.Now;
+            var endDate = DateTime.Now.AddMinutes(10);
+
+            // Set dates to their parsed dates to make sure they are equal to how they will
+            // be read by the query string
+            startDate = DateTime.Parse(startDate.ToString());
+            endDate = DateTime.Parse(endDate.ToString());
+
+            var response = _browser.Get(path, with =>
+            {
+                with.HttpRequest();
+                with.Query("start", startDate.ToString());
+                with.Query("end", endDate.ToString());
+                with.Query("metrics", metric.ToParsableString());
+            });
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Incorrect status code returned");
+            _storageEngine.Verify(x => x.ExecuteQuery(It.Is<Query>(y => y.StartDate == startDate)), Times.Once());
+            _storageEngine.Verify(x => x.ExecuteQuery(It.Is<Query>(y => y.EndDate == endDate)), Times.Once());
+            _storageEngine.Verify(x => x.ExecuteQuery(It.Is<Query>(y => y.Metrics.Contains(metric))), Times.Once());
+        }
+
+        [TestMethod]
+        public void Query_Returns_Results_From_Storage_Engine()
+        {
+            var metric1 = new Metric("device", "category", "name", "suffix");
+            var value1 = 3m;
+            var date1 = DateTime.Parse(DateTime.Now.ToString());
+            var expectedResults = new QueriedMetricResult
+            {
+                Metric = metric1,
+                Values = new[] {new KeyValuePair<DateTime, decimal>(date1, value1)}
+            };
+
+            _storageEngine.Setup(x => x.ExecuteQuery(It.IsAny<Query>()))
+                          .Returns(new[] {expectedResults});
+
+            var response = _browser.Get("/metrics/query", with => with.HttpRequest());
+            var result = JsonConvert.DeserializeObject<QueriedMetricResult[]>(response.Body.AsString());
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Incorrect status code returned");
+            Assert.IsNotNull(result, "Returned result was not a valid QueriedMetricResult array");
+            Assert.AreEqual(1, result.Length, "Returned array had an incorrect number of elements");
+            Assert.AreEqual(metric1, result[0].Metric, "Returned array's metric was incorrect");
+            Assert.AreEqual(1, result[0].Values.Length, "Value array had an incorrect number of elements");
+            Assert.AreEqual(date1, result[0].Values[0].Key, "Value array's date was incorrect");
+            Assert.AreEqual(value1, result[0].Values[0].Value, "Value array's value was incorrect");
         }
     }
 }
