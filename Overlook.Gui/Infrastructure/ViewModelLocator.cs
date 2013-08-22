a0245c58104893aa0916ffb913ce7e7a59bb2953
KallDrexx <me@mshapiro.net>
2013-08-22 19:29:44 -0400

    Created skeleton for the GUI project

---
+++ Overlook.Gui/Infrastructure/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using Overlook.Gui.ViewModels;
+using TinyIoC;
+
+namespace Overlook.Gui.Infrastructure
+{
+    public class ViewModelLocator
+    {
+        private readonly TinyIoCContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = new TinyIoCContainer();
+        }
+
+        // Available View Models
+        public WorkAreaViewModel WorkAreaViewModel { get { return _container.Resolve<WorkAreaViewModel>(); } }
+    }
+}
